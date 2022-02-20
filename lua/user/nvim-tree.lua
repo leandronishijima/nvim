607ce5f12529d1835af79ba15429605081d8200f
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-02-19 22:54:51 -0300

    fixes nvim-tree errors

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -26,8 +26,7 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config") if not config_status_ok then
   return
 end
 
@@ -86,9 +85,9 @@ nvim_tree.setup {
     mappings = {
       custom_only = false,
       list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
+        { key = { "l", "<CR>", "o" }, action =  "edit" },
+        { key = "h", action = "close_node" },
+        { key = "v", action = "vsplit" },
       },
     },
     number = false,
