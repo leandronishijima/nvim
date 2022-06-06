261c61c1fb83db3cda5af5061bdd41ed68652a0e
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-06-06 17:38:10 -0300

    add solargraph manual config

---
+++ lua/user/lsp/settings/solargraph.lua
@@ -0,0 +1,10 @@
+return {
+  settings = {
+    solargraph = {
+      commandPath = '/Users/leandronishijima/.asdf/shims/solargraph',
+      diagnostics = true,
+      completion = true
+    }
+  },
+
+}
