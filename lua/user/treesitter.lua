1343c7891d743b8f3e22d89698f27fdaa5735127
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-06-11 15:02:34 -0300

    removing ignore_install for elixir (treesitter)

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,13 +6,13 @@ end
 configs.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "elixir", "phpdoc" }, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "elixir" }, -- list of language that will be disabled
+    disable = {}, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml" } },
