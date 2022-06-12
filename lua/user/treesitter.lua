d139528184bc66e354dc2e8b0be529d006e32768
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-06-12 09:23:24 -0300

    ensure_installed list (treesitter)

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,24 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "elixir",
+    "ruby",
+    "erlang",
+    "python",
+    "lua",
+    "bash",
+    "markdown",
+    "html",
+    "css",
+    "json",
+    "scss",
+    "heex",
+    "javascript",
+    "dockerfile",
+    "vim"
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   autopairs = {
