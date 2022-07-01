af662b20f963f0d226c2c4cc3b314e608b48b494
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-07-01 16:06:55 -0300

    +vim-endwise

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -130,6 +130,8 @@ return packer.startup(function(use)
 
   use "p00f/nvim-ts-rainbow"
 
+  use "tpope/vim-endwise"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
