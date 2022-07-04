30cf24d977caa5389cc0e041fb7f1bd193aeaf25
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-07-04 16:25:23 -0300

    +options autoindent, showcmd

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,7 +44,10 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
 vim.cmd "set ignorecase"
+vim.cmd "set autoindent"
+vim.cmd "set showcmd"
 
 -- cursor configs
 vim.cmd "set guicursor=i:ver1"
