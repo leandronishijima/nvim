ad09c1e94fad0110ec4656b62560366347cbf03c
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-07-04 23:29:26 -0300

    add publishDiagnostics

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -44,6 +44,17 @@ M.setup = function()
 		border = "rounded",
 		width = 60,
 	})
+
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      -- This sets the spacing and the prefix, obviously.
+      virtual_text = {
+        spacing = 4,
+        prefix = ''
+      }
+    }
+  )
 end
 
 local function lsp_highlight_document(client)
