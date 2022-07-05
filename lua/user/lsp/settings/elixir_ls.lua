f4d51860b1e2429c2c71861d8ab6d338a2ea497d
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-07-04 23:58:40 -0300

    add elixir_ls config and add to lsp-installer config flow

---
+++ lua/user/lsp/settings/elixir_ls.lua
@@ -0,0 +1,10 @@
+return {
+  cmd = { "/Users/leandro/workspace/elixir-ls/language_server.sh" },
+  flags = {
+    debounce_text_changes = 150,
+  },
+  elixirLS = {
+    dialyzerEnabled = false,
+    fetchDeps = false,
+  };
+}
