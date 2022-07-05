c42aa608029de90b76aa52f3d870800deb0c1f51
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-07-05 00:28:31 -0300

    change theme of telescope.git_files()

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -93,7 +93,7 @@ local mappings = {
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
-    "<cmd>lua require('telescope.builtin').git_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    "<cmd>lua require('telescope.builtin').git_files()<cr>",
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
