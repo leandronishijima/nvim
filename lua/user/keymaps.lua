4b5fed6e957059479449af12b53fced7fce2786b
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-08-11 09:16:54 -0300

    add more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -25,6 +25,9 @@ keymap("n", "dW", 'vb"_d', opts)
 -- Select All
 keymap("n", "<C-a>", "gg<S-v>G", opts)
 
+-- Quit buffer
+keymap("n", "<Space>q", ":q", opts)
+
 -- Split buffers
 keymap("n", "ss", ":split<Return><C-w>w", opts)
 keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
@@ -36,18 +39,18 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -1<CR>", opts)
+keymap("n", "<C-Down>", ":resize +3<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -1<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +3<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", "<Esc>:m .+2<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-1<CR>==gi", opts)
 
 -- esc to hide matches
 keymap("n", "<Esc>", ":noh<CR>", term_opts)
@@ -65,16 +68,16 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m .+2<CR>==", opts)
+keymap("v", "<A-k>", ":m .-1<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+2<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-1<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+2<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-1<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
