4766f1985005d637bc5278567893ff84693fc836
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-08-11 09:10:31 -0300

    aligns

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -45,13 +45,13 @@ M.setup = function()
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-      underline = true,
-      -- This sets the spacing and the prefix, obviously.
-      virtual_text = {
-        spacing = 4,
-        prefix = ''
-      }
+    underline = true,
+    -- This sets the spacing and the prefix, obviously.
+    virtual_text = {
+      spacing = 4,
+      prefix = ''
     }
+  }
   )
 end
 
