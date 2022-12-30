3ce6044bbc273e696fe30a8b9078dfe931bacb1b
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-12-30 15:04:39 -0300

    lspinstaller to mason.nvim (#12)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -76,7 +76,10 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use { "williamboman/mason.nvim" }
+  use { "williamboman/mason-lspconfig.nvim" }
+
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "RRethy/vim-illuminate"
 
